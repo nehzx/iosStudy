41d22f7c35dbe4826b449c264e46b0ec0d70a55a
AZName <964190389@qq.com>
2017-11-28 22:24:08 +0800

    feat(*): 物体识别

--- MLDay1/MLDay1/ViewController.m
+++ MLDay1/MLDay1/ViewController.m
@@ -17,6 +17,15 @@
  */
 @property (nonatomic, strong)Resnet50 * resnet;
 
+/**
+ 结果
+ */
+@property (nonatomic, strong)ARHitTestResult * hitTestResult;
+
+/**
+ 分析结果
+ */
+@property (nonatomic, strong)NSMutableArray <VNRequest*> * visionRequests;
 @end
 
     
@@ -35,7 +44,99 @@
     SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/ship.scn"];
     
     // Set the scene to the view
-    self.sceneView.scene = scene;
+    self.sceneView.scene = [[SCNScene alloc] init];
+    
+    
+    [self regiterGestureRecognizers];
+    //创建一个手势
+}
+
+- (void)regiterGestureRecognizers {
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapGesWith:)];
+    [self.view addGestureRecognizer:tap];
+}
+
+- (void)tapGesWith:(UITapGestureRecognizer *)sender {
+    CGPoint touchPoint = self.sceneView.center;
+    //获取图片
+    ARFrame *currentFrame = self.sceneView.session.currentFrame;
+    //识别物体
+    NSArray *hitTestResults = [self.sceneView hitTest:touchPoint types:ARHitTestResultTypeFeaturePoint];
+    if (hitTestResults.count) {
+        self.hitTestResult = hitTestResults.firstObject;
+    }
+    [self perfomVisionRequestWithPixelBuffer:currentFrame.capturedImage];
+}
+
+- (void)displayPredictionsWithText:(NSString *)text {
+    SCNNode *node = [self createTextWith:text];
+    
+    
+    node.position = SCNVector3Make(self.hitTestResult.worldTransform.columns[3].x,
+                                   self.hitTestResult.worldTransform.columns[3].y,
+                                self.hitTestResult.worldTransform.columns[3].z);
+    [self.sceneView.scene.rootNode addChildNode:node];
+}
+
+- (SCNNode *)createTextWith:(NSString *)text {
+    
+    SCNNode *parentNode = [[SCNNode alloc]init];
+    //创建一个球
+    SCNSphere *sphere = [SCNSphere sphereWithRadius:0.1];
+    SCNMaterial *sphereMaterial = [[SCNMaterial alloc]init];
+    sphereMaterial.diffuse.contents = [UIColor yellowColor];
+    sphere.firstMaterial = sphereMaterial;
+    SCNNode *sphereNode = [SCNNode nodeWithGeometry:sphere];
+    // 创建一个文字
+    SCNText *textGeo = [SCNText textWithString:text extrusionDepth:0];
+    textGeo.alignmentMode = kCAAlignmentCenter;
+    textGeo.firstMaterial.diffuse.contents = [UIColor yellowColor];
+    textGeo.firstMaterial.specular.contents = [UIColor whiteColor];
+    textGeo.firstMaterial.doubleSided = YES;
+    textGeo.font = [UIFont systemFontOfSize:0.15];
+    SCNNode *textNode = [SCNNode nodeWithGeometry:textGeo];
+    textNode.scale = SCNVector3Make(0.2, 0.2, - 0.2);
+    //添加到这个几点中
+    [parentNode addChildNode:sphereNode];
+    [parentNode addChildNode:textNode];
+    return parentNode;
+}
+
+
+- (void)perfomVisionRequestWithPixelBuffer:(CVPixelBufferRef)pixelBuffer {
+    
+    //加载 model
+    NSError *error;
+    VNCoreMLModel *visionModel = [VNCoreMLModel modelForMLModel:self.resnet.model error:&error];
+    VNCoreMLRequest *request = [[VNCoreMLRequest alloc]initWithModel:visionModel completionHandler:^(VNRequest * _Nonnull request, NSError * _Nullable error) {
+        
+        if (request.results.count > 0) {
+            VNClassificationObservation *obrervations = request.results.firstObject;
+            
+            
+            
+            NSLog(@"打印结果%@--几率%f",obrervations.identifier, obrervations.confidence);
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                [ self displayPredictionsWithText:obrervations.identifier];
+            });
+        }
+    }];
+    
+    
+    request.imageCropAndScaleOption = VNImageCropAndScaleOptionCenterCrop;
+    [self.visionRequests addObject:request];
+    
+    
+    //翻转结果
+    VNImageRequestHandler * requesetHandler = [[VNImageRequestHandler alloc] initWithCVPixelBuffer:pixelBuffer orientation:kCGImagePropertyOrientationUpMirrored options:@{}];
+
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        //处理结果
+        [requesetHandler performRequests:self.visionRequests error:nil];
+    });
+
+    
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -50,6 +151,8 @@
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
+    
+    
     
     // Pause the view's session
     [self.sceneView.session pause];
@@ -89,10 +192,18 @@
 }
 
 - (Resnet50 *)resnet {
-    if (_resnet) {
+    if (!_resnet) {
         _resnet = [[Resnet50 alloc]init];
     }
     return _resnet;
+}
+
+
+- (NSMutableArray<VNRequest *> *)visionRequests {
+    if (!_visionRequests) {
+        _visionRequests = [NSMutableArray array];
+    }
+    return _visionRequests;
 }
 
 @end
