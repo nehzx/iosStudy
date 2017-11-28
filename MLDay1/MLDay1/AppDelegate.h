9c33ec54fc28dcaa54c1e3e66dedfb67d42d4f94
AZName <964190389@qq.com>
2017-11-28 20:08:50 +0800

    feat: 第一次机器学期

---
+++ MLDay1/MLDay1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLDay1
+//
+//  Created by 徐振 on 2017/11/28.
+//  Copyright © 2017年 xuzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLDay1/MLDay1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLDay1
+//
+//  Created by 徐振 on 2017/11/28.
+//  Copyright © 2017年 xuzhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+#import <Vision/Vision.h>
+@interface ViewController : UIViewController
+
+@end
